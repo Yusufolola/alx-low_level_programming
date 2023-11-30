7096e876146940e80367dbddbec32d37252c06b6
yusufolola <yusufoyesanmi@yahoo.com>
2023-11-29 22:49:36 -0800

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,16 +14,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-									return (0);
-								buf = malloc(sizeof(char) * letters);
-									t = read(fd, buf, letters);
-									w = write(STDOUT_FILENO, buf, t);
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-									free(buf);
-									close(fd);
-										return (w);
+	free(buf);
+	close(fd);
+	return (w);
 }
 
 
